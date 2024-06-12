ad0e56316454eea4b943f3788c589528f7f959fc
MyKnow <myknow000@gmail.com>
2024-06-13 03:58:08 +0900

    [1.0.16+16] iOS CI/CD Cache Test

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,27 +3,53 @@
 # Fail this script if any subcommand fails.
 set -e
 
-# The default execution directory of this script is the ci_scripts directory.
-cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+# Change working directory to the root of your cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH
+
+# Restore Flutter and CocoaPods caches
+echo "Restoring caches..."
+if [ -d "$HOME/flutter_cache" ]; then
+  cp -r $HOME/flutter_cache $HOME/.pub-cache
+fi
+if [ -d "$HOME/cocoapods_cache" ]; then
+  cp -r $HOME/cocoapods_cache ~/.cocoapods
+fi
 
 # Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+if [ ! -d "$HOME/flutter" ]; then
+  git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+fi
 export PATH="$PATH:$HOME/flutter/bin"
 
-# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+# Precache Flutter artifacts for iOS.
 flutter precache --ios
 
 # Install Flutter dependencies.
 flutter pub get
 
+# Cache Flutter dependencies
+echo "Caching Flutter dependencies..."
+mkdir -p $HOME/flutter_cache
+cp -r $HOME/.pub-cache $HOME/flutter_cache
+
 # Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+HOMEBREW_NO_AUTO_UPDATE=1
 brew install cocoapods
 
+# Decrypt configuration files
 openssl aes-256-cbc -d -pbkdf2 -in ios/Runner/GoogleService-Info.plist.enc -out ios/Runner/GoogleService-Info.plist -k $GOOGLE_SERVICES_PASSWORD
 openssl aes-256-cbc -d -pbkdf2 -in .env.enc -out .env -k $ENV_PASSWORD
 
 # Install CocoaPods dependencies.
-cd ios && pod install # run `pod install` in the `ios` directory.
+cd ios
+pod install
+
+# Cache CocoaPods dependencies
+echo "Caching CocoaPods dependencies..."
+mkdir -p $HOME/cocoapods_cache
+cp -r ~/.cocoapods $HOME/cocoapods_cache
+
+# Build iOS app
+flutter build ios --release
 
 exit 0
